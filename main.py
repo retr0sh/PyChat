4f1dd5604da9a068758090db3c6b0faeaa848b69
MSI <kirillka.pk@gmail.com>
2026-01-30 16:58:01 +1200

    alpha 1.2

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ from tkinter import ttk
 
 def set_user_name():
     global user_name
-    if user_name_box.get() == "" or user_name_box.get() == "admin":
+    time_name = user_name_box.get().strip()
+    if time_name == "" or user_name_box.get() == "admin":
         pass
     else:
         user_name = user_name_box.get()
+        massage_window.config(state="normal")
+        massage_window.insert(END, user_name + " joined to chat.\n")
+        massage_window.config(state="disabled")
+
+        online_users_box.config(state="normal")
+        online_users_box.insert(END, user_name + "\n")
+        online_users_box.config(state="disabled")
+
         user_name_box.config(state="disabled")
         user_name_btn.config(state="disabled")
         massage_box.config(state="normal")
@@ -20,7 +29,9 @@ def send_massage():
     if massage_box.get() == "":
         pass
     else:
-        print(user_name, ":", massage_box.get())
+        massage_window.config(state="normal")
+        massage_window.insert(END, user_name + ": " + massage_box.get() + "\n")
+        massage_window.config(state="disabled")
         massage_box.delete(0, END)
 
 
@@ -39,8 +50,9 @@ root.grid_rowconfigure(1, weight=1)
 title = Label(root, text="PyChat", font=("Arial", 14, "bold"))
 title.place(x=10, y=0)
 
-user_name_label = Label(root, text="User Name:")
-user_name_label.place(x=300, y=5)
+
+user_name_label = Label(root, text="User Name:", font=("Arial", 10, "bold"))
+user_name_label.place(x=290, y=5)
 
 user_name_box = ttk.Entry()
 user_name_box.place(x=370, y=5)
@@ -49,7 +61,7 @@ user_name_btn = ttk.Button(root, text="Save", command=set_user_name)
 user_name_btn.place(x=500, y=4)
 
 
-massage_window = Text(width=70, height=20)
+massage_window = Text(width=70, height=20, state="disabled")
 massage_window.place(x=10, y=30)
 
 massage_box = ttk.Entry(state="disabled", width=80)
@@ -57,5 +69,12 @@ massage_box.place(x=10, y=360)
 
 send_btn = ttk.Button(text="Send", state="disabled", command=send_massage)
 send_btn.place(x=500, y=358)
+
+
+online_users = Label(root, text="Online Users:", font=("Arial", 10, "bold"))
+online_users.place(x=615, y=5)
+
+online_users_box = Text(width=20, height=20, state="disabled")
+online_users_box.place(x=580, y=30)
 
 root.mainloop()
